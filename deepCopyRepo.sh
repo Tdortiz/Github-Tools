d35fc44c52ce90405e302217ddb4d215226fcb33
Thomas Ortiz <tdortiz@ncsu.edu>
2017-03-11 01:39:12 -0500

    Added Command Line Arg mode and Documentation
    Can now run the program with the following:
    ./deepCopyRepo.sh
     ./deepCopyRepo.sh <username> <old_repo_url>

--- deepCopyRepo.sh
+++ deepCopyRepo.sh
@@ -1,16 +1,46 @@
 #!/bin/bash
-#
-# Allows for deep copying (including commits/branches)
-# of a github repo. This is useful for moving repos from 
-# one account to another.
-#
-# @author tdortiz (tdortiz@ncsu.edu)
-clear
-printf "This script allows for deep copying (including commits/branches) of a github repo to a new repo.\n\n"
+#####################################################################
+#                           Author                                  #
+#                       tdortiz@ncsu.edu                            #
+#                                                                   #
+#####################################################################
 
-# Retreives user input 
-read -p "Github Username :  " username
-read -p "Repo To Copy url:  " oldRepoUrl
+#####################################################################
+#                                                                   #
+# Allows for deep copying (including commits/branches) of a github  #
+# repo.                                                             #
+#                                                                   #
+# This is useful for moving repos from one account to another.      #
+#                                                                   #
+#####################################################################
+
+#####################################################################
+#                           Running                                 #
+#                     chmod +x deepCopyRepo.sh                      #
+#                                                                   #
+#                       ./deepCopyRepo.sh                           #
+#                               OR                                  #
+#               ./deepCopyRepo.sh <username> <old_repo_url>         #
+#                                                                   #
+#####################################################################
+
+# Clean the terminal
+clear
+printf "This script allows for deep copying (including commits/branches) of a github repo to a new repo.\n"
+printf 'Run Options: "./deepCopyRepo.sh" OR "./deepCopyRepo.sh <username> <old_repo_url>"\n\n'
+
+username=""
+oldRepoUrl=""
+
+# Get input from user or cli
+if [ $# -eq 0 ] 
+then 
+    read -p "Github Username :  " username
+    read -p "Repo To Copy url:  " oldRepoUrl
+else
+    username=$1
+    oldRepoUrl=$2
+fi
 
 # Parse old url for the repo name
 oldRepoName=$(echo ${oldRepoUrl} | cut -d/ -f2- | cut -d/ -f2- | cut -d/ -f2- | cut -d/ -f2- | cut -d. -f1)
